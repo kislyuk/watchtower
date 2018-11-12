ef00c9ff7287a9ee1c342e1c78b547ff3ccbc806
Michael <michael-k@users.noreply.github.com>
2018-11-12 10:31:19 +0000

    Update classifiers

--- setup.py
+++ setup.py
@@ -28,8 +28,9 @@ setup(
         "Operating System :: POSIX",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ]
 )
