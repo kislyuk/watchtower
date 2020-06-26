b9577096f3780b8122ac7cae770093e99e293cfc
François Freitag <mail@franek.fr>
2020-06-26 03:28:27 +0000

    Drop compatibility for Python 2 (#100)
    Python 2 support stopped on 2020-01-01,
    https://devguide.python.org/devcycle/#end-of-life-branches.
    
    Removing support for Python 2.7 reduce testing and maintenance resources
    while allowing the library to move towards modern Python conventions.
    
    - Remove mock test dependency. Use unittest.mock instead.
    - Remove '-*- coding: utf-8 -*-' encoding marker from source files.
      Source files default to utf-8 in Python 3.
    - Remove unnecessary unnecessary __future__ imports.
    - Use Python 3 simplified super() syntax.
    - Use empty format markers `{}` instead of specifying the
      position-specific `{0}`.
    - Drop unicode string marker.
    
    Besides manual inspection, ran the pyupgrade tool to catch additional
    upgrades.

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Watchtower'
-copyright = u'2014, Andrey Kislyuk'
+project = 'Watchtower'
+copyright = '2014, Andrey Kislyuk'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -183,8 +183,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Watchtower.tex', u'Watchtower Documentation',
-   u'Andrey Kislyuk', 'manual'),
+  ('index', 'Watchtower.tex', 'Watchtower Documentation',
+   'Andrey Kislyuk', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -213,8 +213,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'Watchtower', u'Watchtower Documentation',
-     [u'Andrey Kislyuk'], 1)
+    ('index', 'Watchtower', 'Watchtower Documentation',
+     ['Andrey Kislyuk'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'watchtower', u'Watchtower Documentation',
-   u'Andrey Kislyuk', 'Watchtower', 'One line description of project.',
+  ('index', 'watchtower', 'Watchtower Documentation',
+   'Andrey Kislyuk', 'Watchtower', 'One line description of project.',
    'Miscellaneous'),
 ]
 
