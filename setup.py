19120b26162e9924dc818ba16bf09efaa92ef058
Andrey Kislyuk <kislyuk@gmail.com>
2020-06-25 20:46:23 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,8 +27,6 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
