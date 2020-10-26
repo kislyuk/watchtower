43f1ce7ea0092aae75ce960f2955776d11dfcce5
Jon Dufresne <jon.dufresne@gmail.com>
2020-10-26 08:27:00 -0700

    Run flake8 through its own command (#121)
    Fixes warning:
    
    > WARNING: flake8 setuptools integration is deprecated and scheduled for
    > removal in 4.x. For more information, see
    > https://gitlab.com/pycqa/flake8/issues/544
    
    All warnings and errors discovered have been cleaned up.

--- test/run_logging.py
+++ test/run_logging.py
@@ -3,8 +3,9 @@ import os
 import sys
 
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # noqa
-from watchtower import CloudWatchLogHandler
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from watchtower import CloudWatchLogHandler  # noqa: E402
 
 handler = CloudWatchLogHandler(stream_name='run_logging')
 logger = logging.getLogger('run_logging')
