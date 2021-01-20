9b6733d7be142db2e1c685cb10740557579d9d9b
Terence D. Honles <terence@honles.com>
2021-01-20 10:20:04 -0800

    drop support for EOL Python 3.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email="kislyuk@gmail.com",
     description="Python CloudWatch Logging",
     long_description=open("README.rst").read(),
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     install_requires=[
         "boto3 >= 1.9.253, < 2",
     ],
@@ -30,7 +30,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
