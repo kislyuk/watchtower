kislyuk/watchtower

commit ffbc1807400b12ca6ed40a544b148b9cf6ab80c6

Support log group tagging (#201)

watchtower/__init__.py
@@ -182,6 +182,8 @@ class CloudWatchLogHandler(logging.Handler):
         Create CloudWatch Logs log group if it does not exist.  **True** by default.
     :param log_group_retention_days:
         Sets the retention policy of the log group in days.  **None** by default.
+    :param log_group_tags:
+        Tag the log group with the specified tags and values. There is no provision for removing tags. **{}** by default.
     :param create_log_stream:
         Create CloudWatch Logs log stream if it does not exist.  **True** by default.
     :param json_serialize_default:
@@ -211,6 +213,7 @@ class CloudWatchLogHandler(logging.Handler):
         boto3_client: botocore.client.BaseClient = None,
         boto3_profile_name: Optional[str] = None,
         create_log_group: bool = True,
+        log_group_tags: dict[str, str] = {},
         json_serialize_default: Optional[Callable] = None,
         log_group_retention_days: Optional[int] = None,
         create_log_stream: bool = True,
@@ -261,6 +264,9 @@ class CloudWatchLogHandler(logging.Handler):
         if create_log_group:
             self._ensure_log_group()
 
+        if len(log_group_tags) > 0:
+            self._tag_log_group(log_group_tags)
+
         if log_group_retention_days:
             self._idempotent_call(
                 "put_retention_policy", logGroupName=self.log_group_name, retentionInDays=self.log_group_retention_days
@@ -293,6 +299,25 @@ class CloudWatchLogHandler(logging.Handler):
         except self.cwl_client.exceptions.ClientError:
             pass
         self._idempotent_call("create_log_group", logGroupName=self.log_group_name)
+
+    @functools.cache
+    def _get_log_group_arn(self):
+        # get the account number
+        sts_client = boto3.client("sts")
+        accountno = sts_client.get_caller_identity()["Account"]
+        region = self.cwl_client.meta.region_name
+        return f"arn:aws:logs:{region}:{accountno}:log-group:{self.log_group_name}"
+
+    def _tag_log_group(self, log_group_tags: dict[str, str]):
+        try:
+            self._idempotent_call("tag_resource", resourceArn=self._get_log_group_arn(), tags=log_group_tags)
+        except (
+            self.cwl_client.exceptions.ResourceNotFoundException,
+            self.cwl_client.exceptions.InvalidParameterException,
+            self.cwl_client.exceptions.ServiceUnavailableException,
+            self.cwl_client.exceptions.TooManyTagsException,
+        ) as e:
+            warnings.warn(f"Failed to tag log group {self.log_group_name}: {e}", WatchtowerWarning)
 
     def _idempotent_call(self, method, *args, **kwargs):
         method_callable = getattr(self.cwl_client, method)

test/test.py
@@ -317,6 +317,24 @@ class TestPyCWL(unittest.TestCase):
 
         self.assertEqual(submit_batch.call_count, 2)
 
+    def test_log_group_tagging(self):
+
+        tags = {"tag1": "value1", "tag2": "value2"}
+        handler = CloudWatchLogHandler(log_group_name="test_log_group_tagging",
+            log_group_tags=tags)
+        logger = logging.getLogger("test_log_group_tagging")
+        logger.addHandler(handler)
+        logger.propagate = False
+        log_group_arn = handler._get_log_group_arn()
+
+        logs_client = boto3.client("logs")
+
+        logger.critical("This should create a log group with tags")
+
+        tag_list_result = logs_client.list_tags_for_resource(resourceArn=log_group_arn)
+
+        self.assertDictEqual(tag_list_result["tags"], tags)
+
 
 if __name__ == "__main__":
     unittest.main()

commit f0d176ac9696cd01d80196aab5c40459497c4c4a

Fix type errors introduced in #201

watchtower/__init__.py
@@ -11,7 +11,7 @@ import warnings
 from collections.abc import Mapping
 from datetime import date, datetime, timezone
 from operator import itemgetter
-from typing import Any, Callable, List, Optional, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 import boto3
 import botocore
@@ -213,7 +213,7 @@ class CloudWatchLogHandler(logging.Handler):
         boto3_client: botocore.client.BaseClient = None,
         boto3_profile_name: Optional[str] = None,
         create_log_group: bool = True,
-        log_group_tags: dict[str, str] = {},
+        log_group_tags: Dict[str, str] = {},
         json_serialize_default: Optional[Callable] = None,
         log_group_retention_days: Optional[int] = None,
         create_log_stream: bool = True,
@@ -300,7 +300,7 @@ class CloudWatchLogHandler(logging.Handler):
             pass
         self._idempotent_call("create_log_group", logGroupName=self.log_group_name)
 
-    @functools.cache
+    @functools.lru_cache
     def _get_log_group_arn(self):
         # get the account number
         sts_client = boto3.client("sts")
@@ -308,7 +308,7 @@ class CloudWatchLogHandler(logging.Handler):
         region = self.cwl_client.meta.region_name
         return f"arn:aws:logs:{region}:{accountno}:log-group:{self.log_group_name}"
 
-    def _tag_log_group(self, log_group_tags: dict[str, str]):
+    def _tag_log_group(self, log_group_tags: Dict[str, str]):
         try:
             self._idempotent_call("tag_resource", resourceArn=self._get_log_group_arn(), tags=log_group_tags)
         except (